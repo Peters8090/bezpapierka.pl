2c839a85cada77fd564ca292fdaf74d9e493a30c
Peters8090 <bartoszewskip1000@gmail.com>
2020-04-23 20:26:15 +0200

    Create the pages admin. Changes in models

--- backend/pages/admin.py
+++ backend/pages/admin.py
@@ -1,7 +1,61 @@
 from django.contrib import admin
+from nested_inline import admin as nested_inline
+
 from . import models
 
-admin.site.register(models.HomePage)
-admin.site.register(models.OfferPage)
-admin.site.register(models.ContactPage)
-admin.site.register(models.ContentsPage)
+
+# region HomePage
+@admin.register(models.HomePage)
+class HomePageAdmin(nested_inline.NestedModelAdmin):
+    pass
+
+
+# endregion
+
+
+# region OfferPage
+class StepInline(nested_inline.NestedTabularInline):
+    model = models.Step
+    extra = 0
+
+
+class SectionInline(nested_inline.NestedTabularInline):
+    model = models.Section
+    extra = 0
+
+
+class OfferInline(nested_inline.NestedTabularInline):
+    model = models.Offer
+    inlines = [StepInline, SectionInline]
+    extra = 1
+
+
+@admin.register(models.OfferPage)
+class OfferPageAdmin(nested_inline.NestedModelAdmin):
+    inlines = [OfferInline]
+
+
+# endregion
+
+
+# region ContactPage
+class BasicInfoInline(admin.TabularInline):
+    model = models.BasicInfo
+    extra = 0
+
+
+@admin.register(models.ContactPage)
+class ContactPageAdmin(admin.ModelAdmin):
+    inlines = [BasicInfoInline]
+
+
+# endregion
+
+
+# region ContentsPage
+
+@admin.register(models.ContentsPage)
+class ContentsPageAdmin(admin.ModelAdmin):
+    pass
+
+# endregion
--- backend/pages/models.py
+++ backend/pages/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=20, unique=True)
     description = models.CharField(max_length=1000, blank=True)
-    link = models.CharField(max_length=20)
+    link = models.CharField(max_length=20, unique=True)
     exact = models.BooleanField(default=True)
-    icon = models.CharField(max_length=50)
+    icon = models.CharField(max_length=50, unique=True)
 
-    class Meta:
-        abstract = True
+    def __str__(self):
+        return self.title
 
 
 # region HomePage
@@ -27,8 +27,9 @@ class OfferPage(Page):
 
 
 class Offer(models.Model):
-    title = models.CharField(max_length=20)
-    slug = models.SlugField()
+    offer_page = models.ForeignKey(OfferPage, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(unique=True)
     superscription = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
     image = models.ImageField(upload_to='pages/offer_page/offers')
@@ -36,13 +37,13 @@ class Offer(models.Model):
 
 class Step(models.Model):
     offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, unique=True)
     description = models.CharField(max_length=500)
 
 
 class Section(models.Model):
     offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, unique=True)
     contents = models.CharField(max_length=2000)
 
 
@@ -56,8 +57,8 @@ class ContactPage(Page):
 
 class BasicInfo(models.Model):
     contact_page = models.ForeignKey(ContactPage, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    icon = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
+    icon = models.CharField(max_length=50, unique=True)
 
 
 # endregion
@@ -65,6 +66,6 @@ class BasicInfo(models.Model):
 
 # region ContentsPage
 class ContentsPage(Page):
-    contents = models.CharField(max_length=2000)
+    contents = models.CharField(max_length=2000, unique=True)
     image = models.ImageField(upload_to='pages/contents_page/image')
 # endregion
