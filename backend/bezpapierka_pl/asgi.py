d66c6912d8a7a60f40d745674a6788a345c69bc3
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-04-23 16:02:23 +0200

    Move the React project to 'frontend' folder. Create a new Django project inside 'backend' folder

---
+++ backend/bezpapierka_pl/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bezpapierka_pl project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bezpapierka_pl.settings')
+
+application = get_asgi_application()
