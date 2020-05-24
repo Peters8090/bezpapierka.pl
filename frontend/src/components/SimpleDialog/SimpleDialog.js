bbfd239a34c13cc5970a9c59cb8fe309c923d510
Peters8090 <bartoszewskip1000@gmail.com>
2020-05-24 19:59:26 +0200

    Add a hiding backdrop functionality to the SimpleDialog.js

--- frontend/src/components/SimpleDialog/SimpleDialog.js
+++ frontend/src/components/SimpleDialog/SimpleDialog.js
@@ -4,11 +4,16 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
+import useTheme from '@material-ui/core/styles/useTheme';
+import Tooltip from '@material-ui/core/Tooltip';
 import React, {useState} from 'react';
 import uniqid from 'uniqid';
 import Draggable from 'react-draggable';
 import {SlideTransition} from '../../utility';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 /** @jsx jsx */
 import {jsx, css} from '@emotion/core';
 
@@ -24,17 +29,35 @@ export const SimpleDialog = ({
       <div/>, draggable = false,
 }) => {
   const [handleId] = useState(uniqid());
+  const [hideBackdrop, setHideBackdrop] = useState(false);
+
+  const theme = useTheme();
+
   return (
       <Dialog open={open}
               fullWidth={children !== undefined}
               TransitionComponent={SlideTransition}
               PaperComponent={draggable ? PaperComponent : undefined}
               PaperProps={{handleId: handleId}}
+              hideBackdrop={hideBackdrop}
               keepMounted
               onClose={() => setOpen(false)}>
         {React.cloneElement(dialogWrapper, {
           children: (
               <React.Fragment>
+                <Tooltip title="Przytrzymaj, aby podejrzeć zmiany">
+                  <IconButton onMouseDown={() => setHideBackdrop(true)}
+                              onMouseUp={() => setHideBackdrop(false)}
+                              disableRipple
+                              style={{
+                                position: 'absolute',
+                                bottom: theme.spacing(1),
+                                left: theme.spacing(1),
+                              }}>
+                    {hideBackdrop ? <VisibilityOffIcon/> : <VisibilityIcon/>}
+                  </IconButton>
+                </Tooltip>
+
                 <DialogTitle id={handleId}
                              css={{
                                cursor: draggable && 'move',
